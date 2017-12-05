d8e710f364c8e775ef3b690a50dc5df717319ec9
Olegzandr <alex@oresoftware.com>
2017-12-04 17:29:56 -0800

    added lock files

--- test/fixtures/models/bs-test-model.js
+++ test/fixtures/models/bs-test-model.js
@@ -1,123 +1,116 @@
 const util = require('util');
 const F = require('../../../lib/frankenstop-model');
 
-
 const Duck = F.bestow(function (obj, isPreValidate) {
-
-    this.duckId = '???';
-    this.duckTypeId = obj.duckTypeId || '???';
-    this.duckSiblings = obj.duckSiblings;
-
-    //this may throw an error, for purposes of fa
-    // iling-fast for devs
-    F.apply(this, [isPreValidate]);
+  
+  this.duckId = '???';
+  this.duckTypeId = obj.duckTypeId || '???';
+  this.duckSiblings = obj.duckSiblings;
+  
+  //this may throw an error, for purposes of failing-fast for devs
+  F.call(this, isPreValidate);
 });
 
-
-Duck.fromExisting = function _fromExisting(obj){
-
-     if(!obj.duckId){
-         throw new Error('Duck has no duckId');
-     }
-     else{
-         return new Duck(obj);
-     }
-
+Duck.fromExisting = function (obj) {
+  
+  if (!obj.duckId) {
+    throw new Error('Duck has no duckId');
+  }
+  else {
+    return new Duck(obj);
+  }
+  
 };
 
 // do the inheritance thing
 
-
 Duck.getSchema = function getDuckSchema() {
-
-    return Object.freeze({
-
-        prevalidateAllFields: true,
-        allowExtraneousProperties: false,
-
-        properties: {
-
-            duckId: {
-                type: 'uid',
-                primaryKey: true,
-                required: false
+  
+  return Object.freeze({
+    
+    prevalidateAllFields: true,
+    allowExtraneousProperties: false,
+    
+    properties: {
+      
+      duckId: {
+        type: 'uid',
+        primaryKey: true,
+        required: false
+      },
+      
+      duckTypeId: {
+        type: 'uid',
+        required: false
+      },
+      
+      duckSiblings: {
+        type: 'array',
+        required: true,
+        elements: {
+          minLength: null,
+          maxLength: 100,
+          properties: {
+            colors: {
+              type: 'string',
+              required: false
             },
-
-            duckTypeId: {
-                type: 'uid',
-                required: false
+            eatsBugs: {
+              type: 'boolean',
+              required: true
             },
-
-            duckSiblings: {
-                type: 'array',
-                required: true,
-                elements: {
-                    minLength: null,
-                    maxLength: 100,
-                    properties: {
+            children: {
+              type: 'array',
+              required: true,
+              elements: {
+                properties: {
+                  colors: {
+                    type: 'string',
+                    required: false
+                  },
+                  eatsBugs: {
+                    type: 'boolean',
+                    required: true
+                  },
+                  children: {
+                    type: 'array',
+                    required: true,
+                    elements: {
+                      properties: {
                         colors: {
-                            type: 'string',
-                            required: false
+                          type: 'string',
+                          required: false
                         },
                         eatsBugs: {
-                            type: 'boolean',
-                            required: true
-                        },
-                        children: {
-                            type: 'array',
-                            required: true,
-                            elements: {
-                                properties: {
-                                    colors: {
-                                        type: 'string',
-                                        required: false
-                                    },
-                                    eatsBugs: {
-                                        type: 'boolean',
-                                        required: true
-                                    },
-                                    children: {
-                                        type: 'array',
-                                        required: true,
-                                        elements: {
-                                            properties: {
-                                                colors: {
-                                                    type: 'string',
-                                                    required: false
-                                                },
-                                                eatsBugs: {
-                                                    type: 'boolean',
-                                                    required: true
-                                                }
-                                            }
-
-                                        }
-                                    }
-                                }
-
-                            }
+                          type: 'boolean',
+                          required: true
                         }
+                      }
+                      
                     }
-
+                  }
                 }
-
+                
+              }
             }
-
-
+          }
+          
         }
-    })
-
+        
+      }
+      
+    }
+  })
+  
 };
 
-
 Duck.prototype.toRefPath = function () {
-    return '/ducks/' + this.duckId;
+  return '/ducks/' + this.duckId;
 };
 
 Duck.prototype.toJSON = function toJSON() {
 
 };
-
 
 F.validateFrankenstopSchema(Duck);
 module.exports = Duck;
--- test/src/bs-model.test.js
+++ test/src/bs-model.test.js
@@ -1,66 +1,63 @@
 const suman = require('suman');
 const Test = suman.init(module);
 
-
 Test.create(function (fs, path, assert, describe) {
-
-    const Duck = require('../fixtures/models/bs-test-model');
-
-    describe('test valid json', function () {
-
-        const dir = path.resolve(__dirname + '/../fixtures/model-data/valid');
-
-        const duckData =
-            fs.readdirSync(dir).filter(p => String(p).endsWith('.json')).map(p => {
-                return JSON.parse(fs.readFileSync(path.resolve(dir, p)));
-            });
-
-        duckData.forEach(d => {
-
-            console.log('data:', d);
-
-            this.it('constructor test', t => {
-
-                const duck = new Duck(d, true);
-                const errors = duck.validate();
-
-                if (errors.length > 0) {
-                    console.log(errors.map(e => e.stack).join('\n\n'));
-                    throw 'failed';
-                }
-            });
-
-        });
-
+  
+  const Duck = require('../fixtures/models/bs-test-model');
+  
+  describe('test valid json', function (b, it) {
+    
+    const dir = path.resolve(__dirname + '/../fixtures/model-data/valid');
+    
+    const duckData =
+      fs.readdirSync(dir).filter(p => String(p).endsWith('.json')).map(p => {
+        return JSON.parse(fs.readFileSync(path.resolve(dir, p)));
+      });
+    
+    duckData.forEach(d => {
+      
+      console.log('data:', d);
+      
+      it('constructor test', t => {
+        
+        const duck = new Duck(d, true);
+        const errors = duck.validate();
+        
+        if (errors.length > 0) {
+          console.log(errors.map(e => e.stack).join('\n\n'));
+          throw 'failed';
+        }
+      });
+      
     });
-
-
-    describe.skip('test in-valid json', function () {
-
-
-        const dir = path.resolve(__dirname, 'fixtures/bs-test-model-data/invalid');
-
-        const duckData =
-            fs.readdirSync(dir).filter(p => String(p).endsWith('.json')).map(p => {
-                return JSON.parse(fs.readFileSync(path.resolve(dir, p)));
-            });
-
-        duckData.forEach(d => {
-
-            this.it('constructor test', t => {
-
-                assert.throws(function () {
-                    const keys = Object.keys(Duck.getSchema().properties);
-                    new Duck(d, false).preValidate(keys);
-
-                });
-
-            });
-
+    
+  });
+  
+  describe.skip('test in-valid json', function (b, it) {
+    
+    const dir = path.resolve(__dirname, 'fixtures/bs-test-model-data/invalid');
+    
+    const duckData =
+      fs.readdirSync(dir).filter(p => String(p).endsWith('.json')).map(p => {
+        return JSON.parse(fs.readFileSync(path.resolve(dir, p)));
+      });
+    
+    duckData.forEach(d => {
+      
+      it('constructor test', t => {
+        
+        assert.throws(function () {
+          const keys = Object.keys(Duck.getSchema().properties);
+          new Duck(d, false).preValidate(keys);
+          
         });
-
+        
+      });
+      
     });
-
+    
+  });
+  
 });
 
 
